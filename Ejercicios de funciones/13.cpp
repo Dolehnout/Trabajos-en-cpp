1754951a9c468e689efcc8147e91358ca98088af
Dolehnout <105469590+Dolehnout@users.noreply.github.com>
2022-07-07 12:07:30 -0500

    Add files via upload

--- Ejercicios de funciones/13.cpp
+++ Ejercicios de funciones/13.cpp
@@ -1,33 +1,27 @@
 #include <iostream>
 using namespace std;
 
-/*
-13. Diseñe la función que devuelve el resultado de la siguiente serie:
-
-1 - 2 + 3 - 4 + 5 - 6 + 7 - N
-
-N es un parámetro que indica el límite de la serie
-*/
-
-void sumaresta(int pp)
+int sumatorias(int nnn)
 {
-    int sr=1;
-     for (int i=1; i<=pp; i++)
-      if (i%2 ==0)
-          {
-            sr+=i;
-          }            
-       else
-          {
-            sr-=i;
-          }
-    cout << "El resultado es de la secuencia de " <<pp<< " es: " <<sr<< endl;
+    int suma = 0;
+    bool flag = true;
+    for(int i=1; i<=nnn; i++)
+        {
+            if (flag==true)
+                suma = suma + i; //Abreviación && suma += i;
+            else
+                suma = suma - i; //Abreviación && suma-= i;
+
+        flag = !flag;
+        }
+        return suma;
 }
 
 int main ()
 {
-    sumaresta(5);
-
+    int resultados= sumatorias(8);
+    cout<<endl<<"La sumatoria de la serie cuando N es 8 es: ";
+    cout<<resultados;
 
     return 0;
 }
