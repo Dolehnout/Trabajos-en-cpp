4c4557fdcd432865940457cdca9d6342467b4e70
Dolehnout <105469590+Dolehnout@users.noreply.github.com>
2022-07-11 12:55:28 -0500

    Add files via upload

---
+++ game/cartas.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+using namespace std;
+
+
+void corazonNegro3()
+{
+    cout<<"╔═══════════════╗";
+    cout<<endl;
+    cout<<"║3	♠	║";
+    cout<<endl;
+    cout<<"║♠		║";
+    cout<<endl;
+    cout<<"║		║";
+    cout<<endl;
+    cout<<"║		║";
+    cout<<endl;
+    cout<<"║	♠	║";
+    cout<<endl;
+    cout<<"║		║";
+    cout<<endl;
+    cout<<"║		║";
+    cout<<endl;
+    cout<<"║	       ♠║";
+    cout<<endl;
+    cout<<"║	♠      ↋║";
+    cout<<endl;
+    cout<<"╚═══════════════╝";
+}
+
+
+void trebolnegro3()
+{
+    cout<<"╔═══════════════╗";
+    cout<<endl;
+    cout<<"║3	♣	║";
+    cout<<endl;
+    cout<<"║♣		║";
+    cout<<endl;
+    cout<<"║		║";
+    cout<<endl;
+    cout<<"║		║";
+    cout<<endl;
+    cout<<"║	♣	║";
+    cout<<endl;
+    cout<<"║		║";
+    cout<<endl;
+    cout<<"║		║";
+    cout<<endl;
+    cout<<"║	       ♣║";
+    cout<<endl;
+    cout<<"║	♣      ↋║";
+    cout<<endl;
+    cout<<"╚═══════════════╝";
+}
+
+
+void corazon3()
+{
+    cout<<"╔═══════════════╗";
+    cout<<endl;
+    cout<<"║3	♥	║";
+    cout<<endl;
+    cout<<"║♥		║";
+    cout<<endl;
+    cout<<"║		║";
+    cout<<endl;
+    cout<<"║		║";
+    cout<<endl;
+    cout<<"║	♥	║";
+    cout<<endl;
+    cout<<"║		║";
+    cout<<endl;
+    cout<<"║		║";
+    cout<<endl;
+    cout<<"║	       ♥║";
+    cout<<endl;
+    cout<<"║	♥      ↋║";
+    cout<<endl;
+    cout<<"╚═══════════════╝";
+}
+
+
+void diamante3()
+{
+    cout<<"╔═══════════════╗";
+    cout<<endl;
+    cout<<"║3	♦	║";
+    cout<<endl;
+    cout<<"║♦		║";
+    cout<<endl;
+    cout<<"║		║";
+    cout<<endl;
+    cout<<"║		║";
+    cout<<endl;
+    cout<<"║	♦	║";
+    cout<<endl;
+    cout<<"║		║";
+    cout<<endl;
+    cout<<"║		║";
+    cout<<endl;
+    cout<<"║	       ♦║";
+    cout<<endl;
+    cout<<"║	♦      ↋║";
+    cout<<endl;
+    cout<<"╚═══════════════╝";
+}
+
+int main ()
+{
+    corazonNegro3();
+    cout<<endl;
+    trebolnegro3();
+    cout<<endl;
+    corazon3();
+    cout<<endl;
+    diamante3();
+    cout<<endl;
+    
+    return 0;
+}
