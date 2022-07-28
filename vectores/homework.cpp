60d85d00b6367097eeafecd30f4fc575316e7d9e
Dolehnout <105469590+Dolehnout@users.noreply.github.com>
2022-07-28 12:54:33 -0500

    Add files via upload

---
+++ vectores/homework.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+/*
+Tarea:
+Usando vectores se pide:
+1) Declare un vector de 100 enteros para registrar las notas de 100 estudiantes
+2) Ingrese las notas de estos 100 estudiantes. Alternativa: llene el vector con números aleatorios
+3) Calcular la sumatoria y el promedio 
+4) Encontrar la nota máxima 
+*/
+
+int main ()
+{
+
+
+    return 0;
+}
