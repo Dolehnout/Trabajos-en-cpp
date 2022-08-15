44a94dab90e6e08ccb37e5d1d6b791d47f3c2ee3
andr3w56 <andrewgonzale009@gmail.com>
2022-08-15 07:49:33 -0500

    Creating CMakelists

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Administrator/Documents/Trabajos-en-cpp
+# Build directory: C:/Users/Administrator/Documents/Trabajos-en-cpp/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
